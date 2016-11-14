0c6061b8b86b3be80e8904000106fe05757a8021
Ron Waldon <jokeyrhyme@gmail.com>
2016-11-14 12:46:32 +1100

    add "id" to tasks, ensure basic conformity

--- test/tasks.js
+++ test/tasks.js
@@ -10,16 +10,32 @@ const { loadTasks } = require('../lib/tasks.js')
 
 const TASKS_DIR = path.join(__dirname, '..', 'lib', 'tasks')
 
-test('all tasks uniquely loaded', (t) => {
+test('loadTasks()', (t) => {
   return fs.readdir(TASKS_DIR)
     .then((files) => {
       t.true(Array.isArray(files))
 
       const tasks = loadTasks()
       t.is(files.length, tasks.length)
-
-      const labels = tasks.map((task) => task.label)
-      const uniqueLabels = Array.from(new Set(labels))
-      t.is(files.length, uniqueLabels.length)
     })
+})
+
+test('all tasks have unique "id" and "label"', (t) => {
+  const tasks = loadTasks()
+
+  const labels = tasks.map((task) => task.label)
+  const uniqueLabels = Array.from(new Set(labels))
+  t.is(tasks.length, uniqueLabels.length)
+
+  const ids = tasks.map((task) => task.id)
+  const uniqueIds = Array.from(new Set(ids))
+  t.is(tasks.length, uniqueIds.length)
+})
+
+test('all tasks "fn" function', (t) => {
+  const tasks = loadTasks()
+
+  tasks.forEach((task) => {
+    t.is(typeof task.fn, 'function', task.id)
+  })
 })
