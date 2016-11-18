fa0ac01219939fc16babb32655fbabddeb02428a
Ron Waldon <jokeyrhyme@gmail.com>
2016-11-18 13:49:39 +1100

    use eslint-plugin-ava for our own tests

--- test/nodejs-versions.js
+++ test/nodejs-versions.js
@@ -5,9 +5,9 @@ const test = require('ava')
 
 const { fetchVersions, supportedMajors } = require('../lib/nodejs-versions.js')
 
-test('fetchVersions()', (t) => {
-  return fetchVersions()
-    .then((versions) => t.true(Array.isArray(versions)))
+test('fetchVersions()', async (t) => {
+  const versions = await fetchVersions()
+  t.true(Array.isArray(versions))
 })
 
 const versions20161019 = [
