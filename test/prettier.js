2f29abd6515f5468c3a6899639dce81e7b5fe28b
Ron Waldon <jokeyrhyme@gmail.com>
2017-07-25 20:46:32 +1000

    hopefully fix `npm run prettier` test on Windows

--- test/prettier.js
+++ test/prettier.js
@@ -1,15 +1,25 @@
 'use strict';
 
+const os = require('os');
+
 const execa = require('execa');
 const test = require('ava');
 
 test('`npm run prettier` finds correct files', async t => {
-  const result = await execa('npm', [
+  let { stdout } = await execa('npm', [
     'run',
     'prettier',
     '--',
     '--no-list-different'
   ]);
-  t.regex(result.stdout, /test\/prettier\.js/);
-  t.notRegex(result.stdout, /node_modules/);
+
+  t.regex(stdout, /test\/prettier\.js/);
+
+  // output on Windows includes the full path to prettier
+  // so we have to strip it before the next assertion
+  if (/^win/.test(os.platform())) {
+    stdout = stdout.replace('\\node_modules\\prettier\\', '\\');
+  }
+
+  t.notRegex(stdout, /node_modules/);
 });
