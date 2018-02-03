05325bf3b2f4fa0adf696705b6199d3bbe2017f8
Ron Waldon <jokeyrhyme@gmail.com>
2018-02-04 08:37:21 +1100

    use .prettierignore file instead of weird glob

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,8 @@
 
 ## Unreleased
 
+-   use [.prettierignore](https://github.com/prettier/prettier/releases/tag/1.6.0) file instead of a complicated glob
+
 
 ## 3.0.0 - 2017-09-11
 
---
+++ __mocks__/update-json-file.js
@@ -0,0 +1,15 @@
+/* @flow */
+'use strict';
+
+let pkg /* : Object */ = {};
+
+module.exports = async (
+  pkgPath /* : string */,
+  updater /* : (value: any) => Promise<any> */
+) /* : Promise<void> */ => {
+  await updater(pkg);
+};
+
+module.exports.__setPackage = (p /* : Object */) => {
+  pkg = p;
+};
