36f547805229d64a9cb7fe1788b49c533a2a56f3
Ron Waldon <jokeyrhyme@gmail.com>
2018-05-26 07:19:33 +1000

    prettier: quieter, prune unnecessary test

--- __tests__/prettier.js
+++ __tests__/prettier.js
@@ -2,29 +2,12 @@
 
 jest.mock('update-json-file');
 
-const os = require('os');
-
-const execa = require('execa');
-
 const {
   lib: { npmScript },
 } = require('../lib/tasks/prettier.js');
 
 const TEN_SECONDS = 10 * 1e3;
 jest.setTimeout(TEN_SECONDS);
-
-test('`npm run prettier` finds correct files', async () => {
-  let { stdout } = await execa('npm', ['run', 'prettier', '--']);
-
-  // output on Windows includes the full path to prettier
-  // so we have to strip it before the next assertion
-  if (/^win/.test(os.platform())) {
-    stdout = stdout.replace('\\node_modules\\prettier\\', '\\');
-  }
-
-  expect(stdout).not.toMatch(/flow-typed/);
-  expect(stdout).not.toMatch(/node_modules/);
-});
 
 test('npmScript() versus empty package.json', async () => {
   const pkg = {};
